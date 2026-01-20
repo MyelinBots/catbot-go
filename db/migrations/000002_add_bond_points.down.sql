ac453faa317d0a48aa3669e005c49ecb3cf6b28b
James Trotter <james.trotter@joingotu.com>
2026-01-19 19:37:26 -0500

    fix: migrations

--- db/migrations/000002_add_bond_points.down.sql
+++ db/migrations/000002_add_bond_points.down.sql
@@ -4,7 +4,4 @@ ALTER TABLE cat_player
     DROP COLUMN bond_point_streak,
     DROP COLUMN highest_bond_streak,
     DROP COLUMN last_bond_points_at,
-    DROP COLUMN last_interacted_at,
-    DROP COLUMN last_decay_at,
-    DROP COLUMN gifts_unlocked,
-    DROP COLUMN perfect_drop_warned;
+    DROP COLUMN gifts_unlocked;
